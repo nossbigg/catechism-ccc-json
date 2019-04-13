aaf0cec83ea831352b690b69b306f8982d82f732
nossbigg <chenggibson@gmail.com>
2019-04-13 23:24:09 +0800

    Break up footnotes into constitutents

--- src/parsers/footnotesParser.py
+++ src/parsers/footnotesParser.py
@@ -1,6 +1,9 @@
 from collections import namedtuple
+import re
+
 PageFootnotes = namedtuple('PageFootnotes', 'mapping')
-PageFootnote = namedtuple('PageFootnote', 'number text')
+PageFootnote = namedtuple('PageFootnote', 'number refs')
+PageFootnoteRef = namedtuple('PageFootnoteRef', 'text')
 
 
 def extractStructuredFootnotes(raw_nodes):
@@ -11,9 +14,24 @@ def extractStructuredFootnotes(raw_nodes):
         text_node = raw_nodes[index+1]
 
         footnote_number = int(footnote_number_node.text)
-        text = text_node.text
-        text = text.replace("⇒", "").strip()
+        refs = extractFootnoteRefs(text_node.text)
 
-        mapping[footnote_number] = PageFootnote(footnote_number, text)
+        mapping[footnote_number] = PageFootnote(footnote_number, refs)
 
     return PageFootnotes(mapping)
+
+
+def extractFootnoteRefs(footnote_text):
+    ref_texts = footnote_text.split(";")
+    ref_texts = [cleanRefText(t) for t in ref_texts]
+    print(ref_texts)
+    return [PageFootnoteRef(text) for text in ref_texts]
+
+
+def cleanRefText(raw_text):
+    t = raw_text
+    t = t.replace('\n', " ")
+    t = t.replace('\xa0', " ")
+    t = re.sub('[ ]+', " ", t)
+    t = t.strip()
+    return t
